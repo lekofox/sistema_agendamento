93f250099e3675a84a8d7056bc6ef9e445c26cb3
lekofox <leandro_dias9@hotmail.com>
2021-06-09 15:58:24 -0300

    Adição de upload de imagem via post

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,15 +1,29 @@
 import User from "../models/User"
 import jwt from 'jsonwebtoken'
 import authConfig from "../../config/auth";
+import * as Yup from 'yup'
 
 class SessionController {
-    async store (req, res) {
+    async store(req, res) {
+
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6),
+
+        })
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({
+                message: 'Falha na validação'
+            })
+        }
+
         const { email, password } = req.body;
         const user = await User.findOne({ where: { email } })
 
         if (!user) {
             return res.status(401).json({ error: 'Usuário não encontrado' })
-        } 
+        }
 
         if (!(await user.checkPassword(password))) {
             return res.status(401).json({ error: 'Senha inválida' })
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,11 @@ import UserController from './app/controllers/UserController';
 import databaseConfig from './database/index'
 import SessionController from './app/controllers/SessionController'
 import authMiddleware from './app/middlewares/auth'
+import multer from 'multer';
+import multerConfig from './config/multer'
 
 const routes = new Router();
+const upload = multer(multerConfig)
 
 routes.post('/users', UserController.store)
 routes.post('/session', SessionController.store)
@@ -14,5 +17,12 @@ routes.post('/session', SessionController.store)
 routes.use(authMiddleware)
 
 routes.put('/update', UserController.update)
- 
+
+
+// upload
+routes.post('/files',upload.single('file'), (req, res ) => {
+    return res.json({ message: 'Envio concluido com sucesso!'})
+
+})
+
 export default routes;
