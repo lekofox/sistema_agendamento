f47534e403d819863cebc1e6dfc5882529a88712
lekofox <leandro_dias9@hotmail.com>
2021-06-10 22:31:42 -0300

    relacionamento com mongoDB e criaçao de schema

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,6 @@
 import databaseConfig from '../config/database';
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose'
 import User from '../app/models/User';
 import File from '../app/models/File'
 import Appointment from '../app/models/Appointment';
@@ -9,14 +10,22 @@ const models = [User, File, Appointment];
 class Database{
     constructor(){
         this.init() 
-
+        this.mongo()
     }
 
     init(){
         this.connection = new Sequelize(databaseConfig)
         models.forEach(model => model.init(this.connection))
         models.forEach(model => model.associate && model.associate(this.connection.models))
+        
+    }
+    mongo(){
+        this.mongoConnection = mongoose.connect(
+            'mongodb+srv://sistema_agendamento:sistema_agendamento@cluster0.gtlo7.mongodb.net/sistema?retryWrites=true&w=majority',
+            {useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology:true}
+        )
     }
 }
+
 
 export default new Database();
